9340e581b676753ed499cec39da8d0d98d295b04
Pawel Golinski <golpaw1@gmail.com>
2018-10-13 12:17:27 +0200

    Some more code

--- utils/dataset_cleaning.py
+++ utils/dataset_cleaning.py
@@ -18,9 +18,15 @@ class FeatureMetadata(object):
 
 def load_train_dataset(train_csv):
     features_metadata = {}
+    idx_to_feature = {}
     with open(train_csv) as csvfile:
         reader = csv.reader(csvfile)
         for idx, feature in enumerate(next(reader)):
+            idx_to_feature[idx] = feature
             features_metadata[feature] = FeatureMetadata()
             features_metadata[feature].feature_idx = idx
+        for row in reader:
+            print('row is')
+            print(row)
+            break
     return features_metadata
