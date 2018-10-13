5d1e41d3c689439cce4bd29d7bec6ef80a3a36be
Pawel Golinski <golpaw1@gmail.com>
2018-10-13 12:41:07 +0200

    Loading dataset

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-from utils import dataset_cleaning
+from utils import dataset
 
 TRAIN_DATASET = 'data/train.csv'
 
 
 if __name__ == '__main__':
-    metadata = dataset_cleaning.load_train_dataset(TRAIN_DATASET)
-    print(metadata)
-
     # Y_train is array of 0/1 values based on "Prediction" feature (s -> 1, b -> 0)
     # we're ignoring "Id" feature
+
+    X_train, Y_train = dataset.load_train_dataset(TRAIN_DATASET)
+    print(X_train[:10])
+    print(Y_train[:10])
 
     # X_train, Y_train = load_train_dataset(TRAIN_DATASET) NxD
     # X_train, Y_train = clean_dataset("METHOD_NAME", X_train, Y_train)
--- utils/dataset.py
+++ utils/dataset.py
@@ -15,18 +15,33 @@ class FeatureMetadata(object):
         s += "\n)"
         return s
 
+    def string_to_number(self, s):
+        return np.float32(s)
+
 
 def load_train_dataset(train_csv):
+    X_train = []
+    Y_train = []
+
     features_metadata = {}
     idx_to_feature = {}
     with open(train_csv) as csvfile:
         reader = csv.reader(csvfile)
         for idx, feature in enumerate(next(reader)):
+            if feature == 'Id':
+                continue
             idx_to_feature[idx] = feature
             features_metadata[feature] = FeatureMetadata()
             features_metadata[feature].feature_idx = idx
+
         for row in reader:
-            print('row is')
-            print(row)
-            break
-    return features_metadata
+            X_train_row = []
+            for idx, feature_value_str in enumerate(row):
+                if idx in idx_to_feature:
+                    if idx_to_feature[idx] == 'Prediction':
+                        Y_train.append(1.0 if feature_value_str == 's' else 0.0)
+                    else:
+                        X_train_row.append(np.float32(feature_value_str))
+            X_train.append(X_train_row)
+
+    return np.array(X_train), np.array(Y_train)
