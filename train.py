57f3de68759bcf37539b765eb2b736a81e3bbd5d
Josef Weber <josefweber@jw.local>
2018-10-16 21:12:32 +0200

    stochastic gradient descent

--- train.py
+++ train.py
@@ -26,22 +26,30 @@ if __name__ == '__main__':
     # pre process train data
     X_train = dataset.preprocess_dataset(X_train)
 
+    _features = X_train.shape[1]
+    w_initial = np.zeros((_features), dtype=int)
+    max_iters = 5000
 
     '''
     print("Mean Squared Error Gradient Descent")
     # best setting:max_iters 5000  Gradient Descent(14999/14999): gamma=7.4e-07 mse-loss=0.35792074222770376 
-    _features = X_train.shape[1]
-    w_initial = np.zeros((_features), dtype=int)
-    max_iters = 5000
-    gamma = 0.00000074
+    gamma_MSEGD = 0.00000074
     # w is the last optimized vector of the algorithm (len(w)==30)
-    gradient_losses, w = helpers.least_squares_GD(Y_train, X_train, w_initial, max_iters, gamma) 
+    gradient_losses, w = helpers.least_squares_GD(Y_train, X_train, w_initial, max_iters, gamma_MSEGD) 
     '''
 
+    
+    print("Stochastic Mean Squared Error Gradient Descent")
+    # best setting even with batch size = every row .. SGD(2250/4999): loss=0.36466753543609026 super costly
+    gamma_SMSEGD = 0.00000074
+    batch_size = 100000000
+    sgd_losses, w = helpers.least_squares_SGD(Y_train, X_train, w_initial, batch_size, max_iters, gamma_SMSEGD)
+    '''
+    
     print("RIDGE")
     # ridge regression
     w = helpers.ridge_regression(Y_train, X_train, lamda_)
-  
+
     # training error
     loss_train = helpers.compute_loss(Y_train, X_train, w)
     print("Loss for train data is: ", loss_train)
@@ -49,7 +57,7 @@ if __name__ == '__main__':
     # validation error
     loss_val = helpers.compute_loss(Y_validation, X_validation, w)
     print("Loss for validation data is: ", loss_val)
-      
+    '''
 
     # load test data
     Y_test, X_test, indexes = dataset.load_csv_data(TEST_DATA)
